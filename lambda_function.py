9b6c95bbba9939370f5708e50fcfd0af874761d8
andres64372 <77692131+andres64372@users.noreply.github.com>
2022-09-27 15:24:36 -0500

    fixed cors

--- lambda_function.py
+++ lambda_function.py
@@ -22,6 +22,16 @@ def lambda_handler(event, context):
     method = event['httpMethod']
     query = event['queryStringParameters']
     headers = event['headers']
+    if method == 'OPTIONS':
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps({})
+        }
     if path == '/default/RetroPixelApi/login':
         if method == 'POST':
             return login_post(body)
