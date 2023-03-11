55576d81914ea37b78e5da538644f2920122f30f
Andres Herrera <andres.herrera@southworks.com>
2023-03-11 14:37:16 -0500

    deleted tests

--- test_main.py
+++ test_main.py
@@ -113,90 +113,90 @@ class TestGlobal:
         response = lambda_handler(smarthome_object, None)
         assert response['statusCode'] == 200
 
-    # def test_smarthome_execute_onoff(self):
-    #     smarthome_object = request_object(
-    #         '/default/RetroPixelApi/smarthome',
-    #         body={
-    #             "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
-    #             "inputs": [{
-    #             "intent": "action.devices.EXECUTE",
-    #             "payload": {
-    #                 "commands": [{
-    #                     "devices": [{
-    #                         "id": self.device,
-    #                     }],
-    #                     "execution": [{
-    #                         "command": "action.devices.commands.OnOff",
-    #                         "params": {
-    #                             "on": True
-    #                         }
-    #                     }]
-    #                 }]
-    #             }
-    #             }]
-    #         },
-    #         authorization= f'Bearer {self.token}'
-    #     )
-    #     response = lambda_handler(smarthome_object, None)
-    #     result = {
-    #         "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
-    #         "payload": {
-    #             "commands": [{
-    #                 "ids": [
-    #                     self.device
-    #                 ],
-    #                 "status": "SUCCESS",
-    #                 "states": {
-    #                     "on": True,
-    #                     "online": True
-    #                 }
-    #             }]
-    #         }
-    #     }
-    #     assert json.loads(response['body']) == result
+    def test_smarthome_execute_onoff(self):
+        smarthome_object = request_object(
+            '/default/RetroPixelApi/smarthome',
+            body={
+                "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
+                "inputs": [{
+                "intent": "action.devices.EXECUTE",
+                "payload": {
+                    "commands": [{
+                        "devices": [{
+                            "id": self.device,
+                        }],
+                        "execution": [{
+                            "command": "action.devices.commands.OnOff",
+                            "params": {
+                                "on": True
+                            }
+                        }]
+                    }]
+                }
+                }]
+            },
+            authorization= f'Bearer {self.token}'
+        )
+        response = lambda_handler(smarthome_object, None)
+        result = {
+            "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
+            "payload": {
+                "commands": [{
+                    "ids": [
+                        self.device
+                    ],
+                    "status": "SUCCESS",
+                    "states": {
+                        "on": True,
+                        "online": True
+                    }
+                }]
+            }
+        }
+        assert json.loads(response['body']) == result
 
-    # def test_smarthome_execute_color(self):
-    #     smarthome_object = request_object(
-    #         '/default/RetroPixelApi/smarthome',
-    #         body={
-    #             "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
-    #             "inputs": [{
-    #             "intent": "action.devices.EXECUTE",
-    #             "payload": {
-    #                 "commands": [{
-    #                     "devices": [{
-    #                         "id": self.device,
-    #                     }],
-    #                     "execution": [{
-    #                         "command": "action.devices.commands.ColorAbsolute",
-    #                         "params": {
-    #                             "color": {
-    #                                 "spectrumRGB": 16777215
-    #                             }
-    #                         }
-    #                     }]
-    #                 }]
-    #             }
-    #             }]
-    #         },
-    #         authorization= f'Bearer {self.token}'
-    #     )
-    #     response = lambda_handler(smarthome_object, None)
-    #     result = {
-    #         "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
-    #         "payload": {
-    #             "commands": [{
-    #                 "ids": [
-    #                     self.device
-    #                 ],
-    #                 "status": "SUCCESS",
-    #                 "states": {
-    #                     "color":{
-    #                         "spectrumRGB": 16777215
-    #                     },
-    #                     "online": True
-    #                 }
-    #             }]
-    #         }
-    #     }
-    #     assert json.loads(response['body']) == result
+    def test_smarthome_execute_color(self):
+        smarthome_object = request_object(
+            '/default/RetroPixelApi/smarthome',
+            body={
+                "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
+                "inputs": [{
+                "intent": "action.devices.EXECUTE",
+                "payload": {
+                    "commands": [{
+                        "devices": [{
+                            "id": self.device,
+                        }],
+                        "execution": [{
+                            "command": "action.devices.commands.ColorAbsolute",
+                            "params": {
+                                "color": {
+                                    "spectrumRGB": 16777215
+                                }
+                            }
+                        }]
+                    }]
+                }
+                }]
+            },
+            authorization= f'Bearer {self.token}'
+        )
+        response = lambda_handler(smarthome_object, None)
+        result = {
+            "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
+            "payload": {
+                "commands": [{
+                    "ids": [
+                        self.device
+                    ],
+                    "status": "SUCCESS",
+                    "states": {
+                        "color":{
+                            "spectrumRGB": 16777215
+                        },
+                        "online": True
+                    }
+                }]
+            }
+        }
+        assert json.loads(response['body']) == result
