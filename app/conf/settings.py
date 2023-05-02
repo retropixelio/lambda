f178ab06ce7877bb6d0d1688ffab298fca4c6fdd
Andres Herrera <andres64372@hotmail.com>
2023-05-01 20:44:58 -0500

    added secrets

--- app/conf/settings.py
+++ app/conf/settings.py
@@ -1,6 +1,6 @@
-import os
+from decouple import config
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET = os.environ.get('SECRET', 'fake-token')
+SECRET = config('SECRET')
