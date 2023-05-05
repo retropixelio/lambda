5d3ec6b71cde1a9cf42b6fade13a2eb4d44a34dc
Andres Herrera <andres64372@hotmail.com>
2023-05-04 20:30:30 -0500

    mqtt publish on connect

--- app/use_cases/iot.py
+++ app/use_cases/iot.py
@@ -21,19 +21,9 @@ class ConnectedUseCase:
             device_list = self.__firebase.get_fisical(payload.clientId)
             for device in device_list:
                 self.__firebase.update_state(device, 'Online', {'online': True})
-                # socketio.emit(device,{"branch":"Online","id":device,"state":True})
-        if payload.eventType == "publish":
-            device_list = self.__firebase.get_fisical(payload.device)
-            for device in device_list:
                 data = self.__firebase.get_device(device)
                 OnOff = "true" if data.OnOff.on else "false"
                 Color = data.ColorSetting.color.spectrumRGB if data.ColorSetting.color.spectrumRGB else 16777215
-                #self.__mqtt.publish(f"{device}/OnOff",OnOff)
-                #self.__mqtt.publish(f"{device}/Color",str(Color))
-            # if payload["topic"].split('/')[1] == "OnOff":
-            #     id = payload['topic'].split('/')[0]
-            #     socketio.emit(id,{"branch":"OnOff","id":id,"state":True if payload["payload"] == "true" else False})
-            # if payload["topic"].split('/')[1] == "Color":
-            #     id = payload['topic'].split('/')[0]
-            #     socketio.emit(id,{"branch":"Color","id":id,"state":int(payload["payload"])})
+                self.__mqtt.publish(f"{device}/OnOff",OnOff)
+                self.__mqtt.publish(f"{device}/Color",str(Color))
         return response_object({}, 200)
