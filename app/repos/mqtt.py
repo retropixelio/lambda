ab98b1d25b648e9cc229b92f4971d8ae7ac4b938
andresouthworks <andres64372@hotmail.com>
2023-08-24 11:24:05 -0500

    added homegraph support

--- app/repos/mqtt.py
+++ app/repos/mqtt.py
@@ -1,7 +1,10 @@
 import boto3
 from boto3_type_annotations.iot_data import Client
 
-mqtt_client : Client = boto3.client('iot-data')
+mqtt_client : Client = boto3.client(
+    'iot-data',
+    region_name='us-east-1',
+)
 
 class Mqtt:        
     def publish(self,topic,payload):
