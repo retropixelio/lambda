6df8a26b6e840f24e41794bfdcb005422b691fb9
andresouthworks <andres64372@hotmail.com>
2023-08-25 21:28:39 -0500

    fixed device update color

--- app/use_cases/iot.py
+++ app/use_cases/iot.py
@@ -27,18 +27,19 @@ class StateUseCase:
         self.__homegraph = homegraph
     
     def execute(self, state: dict):
-        if state.get('color'):
-            color = state['color']['red']*256*256 + state['color']['green']*256 + state['color']['blue']
-            if state['color']['type'] == 0:
-                state['color'] = {'p': color}
-            elif state['color']['type'] == 1:
-                state['color'] = {'s': color}
-            elif state['color']['type'] == 2:
-                state['color'] = {'t': color}
         device = self.__firebase.get_device(state["deviceId"])
         if device:
             device = device.to_dict()
-            device.update(state)
+            if state.get('color'):
+                color = state['color']['red']*256*256 + state['color']['green']*256 + state['color']['blue']
+                if state['color']['type'] == 0:
+                    device['color']['p'] = color
+                elif state['color']['type'] == 1:
+                    device['color']['s'] = color
+                elif state['color']['type'] == 2:
+                    device['color']['t'] = color
+            else:
+                device.update(state)
             device = Device.from_dict(device)
             self.__firebase.set_state(device)
             for user in device.users:
