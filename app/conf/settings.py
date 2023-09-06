05bf08a6457fa8447affd6e5986ca3cbd5aee896
andresouthworks <andres64372@hotmail.com>
2023-09-05 19:01:57 -0500

    added cast

--- app/conf/settings.py
+++ app/conf/settings.py
@@ -5,5 +5,5 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET = config('SECRET', cast=str)
 PRIVATE_KEY = config('PRIVATE_KEY', cast=str)
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default="")
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default="")
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', cast=str, default='')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', cast=str, default='')
