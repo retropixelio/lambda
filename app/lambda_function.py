3f98a6257e7f2a21472ec025f90c55dc38986e58
andresouthworks <andres64372@hotmail.com>
2023-12-05 21:07:34 -0500

    debug

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -26,7 +26,6 @@ def lambda_handler(event: dict, _):
         return response.get(request)
     if event.get('deviceId'):
         request = DeviceState.from_dict(event)
-        print(request)
         response = StateView()
         return response.get(request)
     elif event.get('path'):
