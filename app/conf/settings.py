39c5ad2545f48d8cd82172dba3cb008d9d01ee3b
andresouthworks <andres64372@hotmail.com>
2023-12-12 12:34:32 -0500

    changed firebase id

--- app/conf/settings.py
+++ app/conf/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET = config('SECRET', cast=str)
 FIREBASE_CERTS = config('GOOGLE_CERTS', cast=str, default='https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com')
-FIREBASE_ID = config('FIREBASE_ID', cast=str, default='retropixel-396819')
+FIREBASE_ID = config('FIREBASE_ID', cast=str, default='retropixel-62580')
 PRIVATE_KEY = config('PRIVATE_KEY', cast=str)
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', cast=str, default='')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', cast=str, default='')
