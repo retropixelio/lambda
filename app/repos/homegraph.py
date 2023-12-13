55b3779e507ec19cbd126a60cf072c3f87cbee73
andresouthworks <andres64372@hotmail.com>
2023-12-12 19:47:03 -0500

    fixed homegraph

--- app/repos/homegraph.py
+++ app/repos/homegraph.py
@@ -1,38 +1,28 @@
 import jwt
 import requests
-from datetime import datetime
+import time
 
 from conf import settings
 from domain.device import Device
 
-epoch_time = datetime(1970, 1, 1)
-iat = int((datetime.utcnow() - epoch_time).total_seconds())
-
+iat = time.time()
+exp = iat + 3600
 credentials = {
-  "iss": "firebase-adminsdk-k11uo@retropixel-396819.iam.gserviceaccount.com",
-  "scope": "https://www.googleapis.com/auth/homegraph",
-  "aud": "https://oauth2.googleapis.com/token",
-  "exp": iat + 3600,
-  "iat": iat
+    'iss': 'firebase-adminsdk-rnn2v@retropixel-96fca.iam.gserviceaccount.com',
+    'sub': 'firebase-adminsdk-rnn2v@retropixel-96fca.iam.gserviceaccount.com',
+    'aud': 'https://homegraph.googleapis.com/',
+    'iat': iat,
+    'exp': exp
 }
-
-secret = "-----BEGIN PRIVATE KEY-----\n" + settings.PRIVATE_KEY.replace("\\n", "\n") + "\n-----END PRIVATE KEY-----\n"
-code = jwt.encode(credentials, secret.encode("utf-8"), 'RS256')
+header = {
+    'kid': '4ea10d0164c2f9ae3b870acadbbe68c1371a615d'
+}
+secret = "-----BEGIN PRIVATE KEY-----\n" + settings.PRIVATE_KEY.replace('\\n', '\n') + "\n-----END PRIVATE KEY-----\n"
+code = jwt.encode(credentials, secret.encode("utf-8"), 'RS256', header)
 
 class HomeGraphRepository:
     def __init__(self):
-        response = requests.post(
-            'https://oauth2.googleapis.com/token',
-            json={
-                "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
-                "assertion": code
-            }
-        )
-        data = response.json()
-        if response.ok:
-            self.__token = data['access_token']
-        else:
-            raise Exception("Homegraph not authenticated")
+        self.__token = code
 
     def request_sync(self, user_id):
         return
@@ -52,25 +42,26 @@ class HomeGraphRepository:
         
     def report_state(self, user_id, device: Device):
         return
-        response = requests.post(
-            'https://homegraph.googleapis.com/v1/devices:reportStateAndNotification',
-            json={
-                "agentUserId": user_id,
-                "payload": {
-                    "devices":{
-                        "states":{
-                            device.device_id:{
-                                "on": device.onoff,
-                                "online": device.online,
-                                "brightness": int(device.brightness * 100 / 255),
-                                "color": {
-                                    "spectrumRGB": device.color.p
-                                }
+        data = {
+            "agentUserId": user_id,
+            "payload": {
+                "devices":{
+                    "states":{
+                        device.id:{
+                            "on": device.onoff,
+                            "online": device.online,
+                            "brightness": int(device.brightness * 100 / 255),
+                            "color": {
+                                "spectrumRGB": device.color.p
                             }
                         }
                     }
                 }
-            },
+            }
+        }
+        response = requests.post(
+            'https://homegraph.googleapis.com/v1/devices:reportStateAndNotification',
+            json=data,
             headers={
                 'Content-Type': 'application/json',
                 'Authorization': f'Bearer {self.__token}'
